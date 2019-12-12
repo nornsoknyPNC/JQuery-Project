cd3714c236a0b36eacbe883be6381c5bfae1ae49
Sokny Norn <sokny.norn@pnc.lan>
2019-12-12 18:20:09 +0700

    add new function

--- js/project.js
+++ js/project.js
@@ -1,15 +1,10 @@
 
-
 function getUrl() {
-    var url ="https://raw.githubusercontent.com/radytrainer/test-api/master/test.json";
+    var url = "https://raw.githubusercontent.com/radytrainer/test-api/master/test.json";
     return url;
 }
-$(document).ready(function() {
+$(document).ready(function () {
     requestApi();
-    $('#recipe').on('change',() => {
-        var chooseRecipe = $('#recipe').val();
-        getRecipe(chooseRecipe);
-    })
 });
 
 function requestApi() {
@@ -20,23 +15,70 @@ function requestApi() {
         error: () => console.log("Cannot request"),
     })
 }
-var getAllData = [];
+var allData = [];
 function chooseRecipe(recipe) {
-    var option ="";
+    var option = "";
     allData = recipe;
     recipe.forEach(element => {
-        option +=`
+        option += `
             <option value="${element.id}">${element.name}</option>
         `;
     });
-    $('#recipe').append(option);
+
+    $('#value').append(option);
+    getRecipe(allData);
 }
 
-// get recipe
-// function getRecipe(recipe) {
-//     switch(recipe) {
-//         case 0:
-//             computeHTML();
-//             break;
-//     }
-// }
+// get recipes and ingredients
+var getRecipe = (recipe) => {
+    recipe.forEach(recs => {
+        $('#value').on('change', () => {
+            var result = $('#value').val();
+            if (recs.id == result) {
+                getRecipes(recs);
+                ingredients(recs);
+                getInstruction(recs.instructions);
+            }
+        })
+    });
+}
+//display recipe 
+var getRecipes = (myData) => {
+    var results = "";
+    results += `
+        <div class="col-3"></div>
+        <div class="col-3"><h3>${myData.name}</h3></div>
+        <div class="col-3"><img src="${myData.iconUrl}" class="img-fluid" width="150"></div>
+        <div class="col-3"></div>
+    `;
+    $('#card').html(results);
+}
+
+// display all ingrediant [name function]
+var ingredients = (getout) => {
+    var display = "";
+    getout.ingredients.forEach(ing => {
+        display += `
+            <tr>
+                <td><img src="${ing.iconUrl}" width="35" class="img-fluid"></td>
+                <td>${ing.quantity} ${ing.unit[0].toLowerCase()}</td>
+                <td>${ing.name}</td>
+            </tr>
+        `;
+        $('#ingredient').html('Ingredients');
+        $('#ing').html(display);
+    })
+}
+// get instruction
+function getInstruction(step) {
+    var result = "";
+    var ing = step.split('<step>');
+    for(let i=1; i<ing.length;i++) {
+        result += `
+            <h5 class="text-primary">Step:${i}<h5>
+            <p>${ing[i]}</p>
+        `;
+    }
+    $('#instructions').html('Instructions');
+    $('#step').html(result);
+}
